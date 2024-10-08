391eab932b245c1bf62e1ebe4927290e3dc0f964
Mackenzie Kormann <mackenziekormann@gmail.com>
2024-10-07 22:47:58 -0700

    Initial implementation of RNaseEChecker and InternalRBSChecker

---
+++ genedesign/checkers/internal_rbs_checker.py
@@ -0,0 +1,105 @@
+import re 
+from models.transcript import Transcript
+from models.rbs_option import RBSOption
+from models.operon import Operon
+
+class InternalRBSChecker:
+    """
+    Description: This class checks sequences for internal RBS binding sites
+    containing a Shine-Dalgarno sequence and a start codon 7-9 bases away that
+    could lead to unintended translation initiation. 
+
+    Inputs (run method):
+        seq (str): An operon DNA sequence
+
+    Output: 
+        Tuple[bool, int, List[int]]: a tuple containing:
+            - internal_rbs_present (bool): False if an internal RBS is present.
+            - num_sites (int): Number of RBSes present in the sequence.
+            - positions (List[int]): List of indices where the RBSes are present.
+    """
+
+    def __init__(self) -> None:
+        """
+        Defines the regex pattern used to check the input for a Shine-Dalgarno 
+        sequence and a start codon.
+        """
+
+        self.rbs_pattern = re.compile(r"AUUAUU[AUCG]{7,9}AUG")
+
+    def run(self, seq):
+        """
+        Checks the input sequence for an RBS based on the pattern.
+
+        Parameters:
+            seq (str): An operon DNA sequence
+
+        Returns: 
+            Tuple containing:
+                - internal_rbs_present (bool)
+                - num_sites (int)
+                - positions (List[int])
+            As defined in the class definition. 
+        """
+
+        seq.replace('T', 'U')
+
+        matches = list(self.rbs_pattern.finditer(seq))
+
+        positions = [match.start() for match in matches]
+
+        num_sites = len(positions)
+
+        if num_sites > 0:
+            internal_rbs_present = False
+        else:
+            internal_rbs_present = True
+
+        return internal_rbs_present, num_sites, positions
+    
+if __name__ == "__main__":
+    """
+    Main method for running the InternalRBSChecker on a hardcoded Operon. 
+    This method initializes InternalRBSChecker, runs it on a hardcoded Operon,
+    annd prints the results including a boolean indicating if an internal RBS is
+    present, the number of sites if they exist, and the position(s) of those sites.
+    """
+
+    internal_rbs_checker = InternalRBSChecker()
+    internal_rbs_checker.initiate()
+
+    rbs1 = RBSOption(
+        utr="AUUAUUACGGAUG",  
+        cds="AUGGCUAAGGAGGAUGCUAA", 
+        gene_name="gene1",
+        first_six_aas="MAKGMC"
+    )
+    rbs2 = RBSOption(
+        utr="AUGGAUUAGGAUUUAA",  
+        cds="AUGGUAUUUAAGGCUAGG", 
+        gene_name="gene2",
+        first_six_aas="MGYAGG"
+    )
+    transcript1 = Transcript(
+        rbs=rbs1,
+        peptide="MAGMC",
+        codons=["AUG", "GCU", "AAG", "GAG", "GCU"]
+    )
+    transcript2 = Transcript(
+        rbs=rbs2,
+        peptide="MGYAG",
+        codons=["AUG", "GUA", "UUU", "AAG", "GCU"]
+    )
+    operon = Operon(
+        transcripts=[transcript1, transcript2],
+        promoter="TTGACA",
+        terminator="TGA"
+    )
+
+    operon_seq = operon.promoter + "".join([transcript.rbs.utr + transcript.rbs.cds for transcript in operon.transcripts]) + operon.terminator
+
+    internal_rbs, num_sites, positions = internal_rbs_checker.run(operon_seq)
+
+    print(f"No internal RBSes are present in the operon: {internal_rbs}")
+    print(f"Number of sites in operon: {num_sites}")
+    print(f"The operon sites are located at: {positions}")
---
+++ genedesign/checkers/rnase_e_checker.py
@@ -0,0 +1,108 @@
+import re
+from models.transcript import Transcript
+from models.rbs_option import RBSOption
+from models.operon import Operon
+
+class RNaseEChecker:
+    """
+    Description: 
+    This class checks sequences for RNase E cleavage sites, which generally come in the form
+    (A/G)AUU(A/U)
+    
+    Input (run method):
+        seq (str): An operon DNA sequence
+
+    Output:
+        Tuple[bool, int, List[int]]: A tuple containing:
+            - rnase_e_site_present (bool): False if an RNase E cleavage site is present
+            - num_sites (int): Number of cleavage sites present in the sequence
+            - positions (List[int]): List of indices where the cleavage sites are present
+    """
+
+    def __init__(self) -> None:
+        self.pattern = None
+
+    def initiate(self) -> None:
+        """
+        Defines the regex pattern used to check the input sequence.
+        """
+
+        self.pattern = re.compile(r"[AU]AUU[AU]")
+
+    def run(self, seq):
+        """
+        Checks the input sequence for cleavage sites based on the RNase E pattern.
+
+        Parameters:
+            seq (str): An operon DNA sequence.
+        
+        Returns: 
+            Tuple containing:
+                - rnase_e_site_present (bool)
+                - num_sites(int)
+                - positions (List[int])
+            As defined in the class definition. 
+        """
+
+        seq.replace('T', 'U')
+
+        matches = list(self.pattern.finditer(seq))
+
+        positions = [match.start() for match in matches]
+
+        num_sites = len(positions)
+
+        if num_sites > 0:
+            rnase_e_site_present = False
+        else:
+            rnase_e_site_present = True
+
+        return rnase_e_site_present, num_sites, positions
+    
+if __name__ == "__main__":
+    """
+    Main method for running the RNaseEChecker on a hardcoded Operon.
+    This method initializes RNaseEChecker, runs it on a hardcoded Operon, 
+    and prints the results including a boolean indicating if an
+    RNase E site is present, the number of sites if they exist, and the position(s) 
+    of those sites. 
+    """
+
+    rnase_e_checker = RNaseEChecker()
+    rnase_e_checker.initiate()
+
+    rbs1 = RBSOption(
+        utr="AGGGAUUAAGGAUAUUAU", 
+        cds="AUGGCUAAGGAGGAUGCUAA", 
+        gene_name="gene1", 
+        first_six_aas="MAKGMC"
+    )
+    rbs2 = RBSOption(
+        utr="AUGGAUUAGGAUUUAA", 
+        cds="AUGGUAUUUAAGGCUAGG", 
+        gene_name="gene2", 
+        first_six_aas="MGYAGG"
+    )
+    transcript1 = Transcript(
+        rbs=rbs1,
+        peptide="MAGMC",
+        codons=["AUG", "GCU", "AAG", "GAG", "GCU"]
+    )
+    transcript2 = Transcript(
+        rbs=rbs2,
+        peptide="MGYAG",
+        codons=["AUG", "GUA", "UUU", "AAG", "GCU"]
+    )
+    operon = Operon(
+        transcripts=[transcript1, transcript2],
+        promoter="TTGACA",
+        terminator="TGA"
+    )
+
+    operon_seq = operon.promoter + "".join([transcript.rbs.utr + transcript.rbs.cds for transcript in operon.transcripts]) + operon.terminator
+
+    site_in_operon, num_operon_sites, operon_site_pos = rnase_e_checker.run(operon_seq)
+
+    print(f"No RNase E cleavage sites are present in the operon: {site_in_operon}")
+    print(f"Number of sites in operon: {num_operon_sites}")
+    print(f"The operon sites are located at: {operon_site_pos}")
